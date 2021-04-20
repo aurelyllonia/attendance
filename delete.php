a0ee3e009bb83f51609be660247eb5e059318d11
Aurel Yllonia <ayllonia@gmail.com>
2021-04-20 20:39:28 +0700

    Initial commit

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'db/conn.php';
+    if(!isset($_GET['id'])){
+        echo 'includes/errormessage.php';
+        header("Location: viewrecords.php");
+    }else{
+        //Get ID values
+        $id = $_GET['id'];
+        //Call Delete function
+        $result = $crud->deleteAttendee($id);
+        //Redirect to list
+        if($result)
+        {
+            header("Location: viewrecords.php");
+        }
+        else{
+            echo '';
+        }
+    }
+?>
